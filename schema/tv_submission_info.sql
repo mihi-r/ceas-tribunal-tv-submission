6fe14738068343a484d90cda42f13314c4f1f082
Jacob Huber <huber2jb@mail.uc.edu>
2020-04-23 19:11:49 -0400

    Fixed errors and added email

--- schema/tv_submission_info.sql
+++ schema/tv_submission_info.sql
@@ -1,56 +1,26 @@
--- phpMyAdmin SQL Dump
--- version 4.8.3
--- https://www.phpmyadmin.net/
---
--- Host: localhost:3306
--- Generation Time: Dec 05, 2019 at 01:32 AM
--- Server version: 5.7.24
--- PHP Version: 7.2.14
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
-
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
---
--- Database: `tribunal`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `tv_submission_info`
---
 
 CREATE TABLE `tv_submission_info` (
   `id` int(11) NOT NULL,
   `admin_name` varchar(256) NOT NULL,
-  `admin_email` varchar(256) NOT NULL
+  `admin_email` varchar(256) NOT NULL,
+  `super_email` varchar(256) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Indexes for dumped tables
---
 
---
--- Indexes for table `tv_submission_info`
---
 ALTER TABLE `tv_submission_info`
   ADD PRIMARY KEY (`id`);
 
---
--- AUTO_INCREMENT for dumped tables
---
 
---
--- AUTO_INCREMENT for table `tv_submission_info`
---
 ALTER TABLE `tv_submission_info`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
--- schema/tv_submissions.sql
+++ schema/tv_submissions.sql
@@ -1,32 +1,13 @@
--- phpMyAdmin SQL Dump
--- version 4.8.3
--- https://www.phpmyadmin.net/
---
--- Host: localhost:3306
--- Generation Time: Dec 05, 2019 at 01:31 AM
--- Server version: 5.7.24
--- PHP Version: 7.2.14
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
-
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
---
--- Database: `tribunal`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `tv_submissions`
---
 
 CREATE TABLE `tv_submissions` (
   `id` int(11) NOT NULL,
@@ -36,23 +17,11 @@ CREATE TABLE `tv_submissions` (
   `description` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Indexes for dumped tables
---
 
---
--- Indexes for table `tv_submissions`
---
 ALTER TABLE `tv_submissions`
   ADD PRIMARY KEY (`id`);
 
---
--- AUTO_INCREMENT for dumped tables
---
 
---
--- AUTO_INCREMENT for table `tv_submissions`
---
 ALTER TABLE `tv_submissions`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
